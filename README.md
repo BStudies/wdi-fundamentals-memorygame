ae327f19b5f7f1b9fecec0cfab7b0b0c158ac094
Brandon <digihew@gmail.com>
2017-06-25 21:01:17 -0400

    upgraded game

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # wdi-fundamentals-memorygame
+
+Todo
+
+add reset button
+submit form via unit 11 assignment bottom
+make reload not refresh the page, simply re apply the pack image
+add all cards?
+unit 6 assignment bonus
+add boarder hover
+use json stringify and json parse to create the objects
+
+
+go to unpack 
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,70 +1,221 @@
 
-var cards = [
-	{
-		rank: "queen",
-		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
+var ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+var suits = ["club","diamond","heart","spade"];
 
-	},
-	{
-		rank: "queen",
-		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
-		
-	},
-	{
-		rank: "king",
-		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
-		
-	},
-	{
-		rank: "king",
-		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
-		
-	},
-];
-
-
-
-var cardsInPlay = [];
-
-//I had to improvise this because the way outlined in the instructions was not working for me.
-var checkForMatch = function(cardId){
-	document.getElementsByTagName("img")[cardId].setAttribute("src",cards[cardId].cardImage);
-
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
-      alert("You found a match!");
-  } else {
-      console.log("Sorry, try again.");
-      alert("Sorry, try again.");
-  }
-}
-
-
-var flipCard = function(){
-	var cardId = this.getAttribute('data-id');
-	checkForMatch(cardId);
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-}
-
-
-var createBoard = function(){
-	for(var i = 0; i < cards.length; ++i){
-		var cardElement = document.createElement('img');
-		cardElement.setAttribute("src","images/back.png");
-		cardElement.setAttribute('data-id',i);
-		cardElement.addEventListener('click',flipCard);
-		document.getElementById('game-board').appendChild(cardElement);
-
+//make deck
+var cards = [];
+for(var k = 0; k < ranks.length; ++k)
+{
+	for(var j = 0; j < suits.length; ++j){
+		//add ranks suits
+		cardsObject = '{"rank": "' + ranks[k] + '","suit": "' + suits[j] + '" ,"cardImage": ' + '"images/all/384px-Playing_card_' + suits[j] + '_' + ranks[k] + '.svg.png' + '"}';
+		cards.push(JSON.parse(cardsObject));
 	}
 }
 
-// flipCard(0);
-// flipCard(2);
-createBoard();
+
+//randomize deck
+var randomizeBoard = function(){
+	var board = document.getElementById('game-board').children;
+	for(var j = 0; j < board.length; ++j){
+		// select 2 indecies
+		var index1 = Math.floor(Math.random()*board.length);
+		
+		//swap them
+		var temp = cards[j];
+		cards[j]=cards[index1];
+		cards[index1]=temp;
+		
+	}
+}
+
+//select difficulty
+
+
+// var cards = [
+// 	{
+// 		rank: "queen",
+// 		suit: "hearts",
+// 		cardImage: "images/queen-of-hearts.png"
+
+// 	},
+// 	{
+// 		rank: "queen",
+// 		suit: "diamonds",
+// 		cardImage: "images/queen-of-diamonds.png"
+		
+// 	},
+// 	{
+// 		rank: "king",
+// 		suit: "hearts",
+// 		cardImage: "images/king-of-hearts.png"
+		
+// 	},
+// 	{
+// 		rank: "king",
+// 		suit: "diamonds",
+// 		cardImage: "images/king-of-diamonds.png"
+		
+// 	},
+// ];
+
+
+
+
+
+var cardsInPlayRank = [];
+var cardsInPlayImages = [];
+var cardsInPlayId = [];
+var matches = [];
+var images = document.getElementsByTagName("img");
+
+
+
+
+//functions
+var checkForMatch = function(mode){
+	if(cardsInPlayId.length == 2){
+		console.log("Cards: " + cards[cardsInPlayId[0]] + " " + cards[cardsInPlayId[1]])
+		if (cards[cardsInPlayId[0]].rank === cards[cardsInPlayId[1]].rank) {
+	      console.log("You found a match!");
+	      alert("You found a match!");
+	      matches.push(cards[cardsInPlayId[0]]);
+	      matches.push(cards[cardsInPlayId[1]]);
+	      cardsInPlayId = [];
+	      //add would you like to play again button.
+	  	} 
+  		else {
+	      console.log("Sorry, try again.");
+	      alert("Sorry, try again.");
+	      unFlipUnmatchedCards();
+	  	}
+	  	
+	  	updateCounter();
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+// when adding event listener this becomes the file, therefore cannot use parameters so need extra functions
+var flipCardEasy = function(){
+	var cardId = this.getAttribute('data-id');
+	if(cardsInPlayId.indexOf(cardId) < 0)
+	{
+		console.log(this);
+		this.setAttribute("src",cards[cardId].cardImage);
+		this.setAttribute("height","279");
+		this.setAttribute("width","175");
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlayId.push(cardId);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+		checkForMatch("easy");
+	}
+	
+}
+var flipCardMedium = function(){
+	var cardId = this.getAttribute('data-id');
+	if(cardsInPlayId.indexOf(cardId) < 0)
+	{
+		console.log(this);
+		this.setAttribute("src",cards[cardId].cardImage);
+		this.setAttribute("height","139");
+		this.setAttribute("width","87");
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlayId.push(cardId);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+		checkForMatch("medium");
+	}
+	
+}
+
+var flipCardHard = function(){
+	var cardId = this.getAttribute('data-id');
+	if(cardsInPlayId.indexOf(cardId) < 0)
+	{
+		console.log(this);
+		this.setAttribute("src",cards[cardId].cardImage);
+		this.setAttribute("height","93");
+		this.setAttribute("width","56");
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlayId.push(cardId);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+		checkForMatch("hard");
+	}
+	
+}
+
+
+
+
+var unFlipUnmatchedCards = function(){
+	var images = document.getElementsByTagName("img");
+	for(var j = 0; j < cardsInPlayId.length; ++j){
+		images[cardsInPlayId[j]].setAttribute("src","images/back.png");
+	}
+	cardsInPlayId = [];
+}
+
+
+
+
+var createCard = function(mode, pixHeight, pixWidth, numberOfCards, i){
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute("src","images/back.png");
+	cardElement.setAttribute('data-id',i);
+	cardElement.setAttribute('height',pixHeight);
+	cardElement.setAttribute('width',pixWidth);
+	switch(mode){
+		case "easy":
+			cardElement.addEventListener('click',flipCardEasy);
+			break;
+		case "medium":
+			cardElement.addEventListener('click',flipCardMedium);
+			break;
+		case "hard":
+			cardElement.addEventListener('click',flipCardHard);
+			break;
+	}
+	document.getElementById('game-board').appendChild(cardElement);
+}
+
+var createBoard = function(mode, pixHeight, pixWidth, numberOfCards){
+	for(var i = 0; i < numberOfCards; ++i){
+		createCard(mode, pixHeight, pixWidth, numberOfCards, i);
+	}
+	randomizeBoard();
+}
+
+
+// need to fix
+var updateCounter = function(){
+	document.getElementsByClassName("counter")[0].innerHTML++;
+}
+
+//easy
+//image heights 279px
+//image width 175px
+
+//medium
+//
+
+//hard
+//image heights 279px
+//image width 175px
+//createBoard();
+
+
+
+
+
+
